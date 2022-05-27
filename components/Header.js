eb8a8283a3936e208ea247779afa98a6325c090e
Mohd Kaif <kaifmohd2014@gmail.com>
2022-05-27 23:02:48 +0530

    Header development

--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,31 @@
 import Link from 'next/link'
 import React from 'react'
 import { GiHamburgerMenu } from 'react-icons/gi'
-import headerlogo from '../assets/headerlogo.png'
-import { BsYoutube } from 'react-icons/bs'
-import { IoMdSearch, IoMdAdd } from 'react-icons/io'
+import logo from '../assets/logo.png'
+import { AiOutlineSearch } from 'react-icons/ai'
 import Image from 'next/image'
 
 const Header = () => {
     return (
-        <div className='flex h-full p-4 bg-[#202020]'>
-            <div className='flex flex-row items-center justify-center'>
-                <GiHamburgerMenu className='w-full text-white' />
-                <BsYoutube className='w-full ml-2 text-red-600' />
-                <h1 className='ml-2 text-white'>Youtube</h1>
+        <div className='shadow-sm border-b bg-[#202020] sticky top-0 z-50'>
+            <div className='flex justify-between bg-[#202020] max-w-6xl mx-5 lg:mx-auto'>
+                <div className='relative hidden cursor-pointer w-80 lg:inline-grid'>
+                    <GiHamburgerMenu className='w-full text-white' />
+                    <Image
+                        src={logo}
+                        layout="fill"
+                        objectFit='contain'
+                        alt='logo'
+                    />
+                </div>
+                <div className='max-w-xs'>
+                    <div className='relative p-3 mt-1 rounded-md '>
+                        <div className='absolute inset-y-0 flex items-center pl-3 pointer-events-none '>
+                            <AiOutlineSearch className='w-5 h-5 text-gray-500 ' />
+                        </div>
+                        <input type="text" className='block w-full pl-10 border-gray-300 rounded-md bg-gray-50 sm:text-sm focus:ring-black focus:border-black' placeholder='Search' />
+                    </div>
+                </div>
             </div>
         </div>
     )
